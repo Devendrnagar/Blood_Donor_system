bc0bd1a5693a1f8dcefb4eb50171c5ef3692104d
Devendrnagar <devendrnagar853@gmail.com>
2025-11-02 16:17:16 +0530

    apiservice update

--- project/src/lib/apiService.ts
+++ project/src/lib/apiService.ts
@@ -2,7 +2,7 @@
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://blood-donor-system-5lbr.onrender.com/api';
 // const API_BASE_URL = 'http://localhost:5000/api';
 
-const API_BASE_URL =  'https://blood-donor-system-5lbr.onrender.com/api';
+const API_BASE_URL =  'https://blood-donor-system-i0et.onrender.com/api';
 interface ApiResponse<T = any> {
   success: boolean;
   message?: string;
